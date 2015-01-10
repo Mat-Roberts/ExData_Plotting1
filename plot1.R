b7a5fc2c5c8c1704adb1007b385dc2f8d409b463
Mat Roberts <mat.roberts@onshoreoutsourcing.com>
2015-01-10 15:40:01 -0600

    Revert "Create plot1.R"
    This reverts commit 052ea7b51591d8839360413ba6a88e5456aae6f8.

--- plot1.R
+++
@@ -1,8 +0,0 @@
-data <- read.table("household_power_consumption.txt", sep=";",header=TRUE,na.strings="?",stringsAsFactors=FALSE)
-data$Date <- as.Date(data$Date, '%d/%m/%Y')
-data2 <- subset(data,data$Date>="2007-02-01" & data$Date<="2007-02-02")
-data2$datetime <- paste(as.character(data2$Date),data2$Time,sep=" ")
-data2$datetime <- strptime(data2$datetime,format="%Y-%m-%d %H:%M:%S") 
-png(file="plot1.png",width=480,height=480)
-hist(data2$Global_active_power,col="red",xlab="Global Active Power (kilowatts)",main="Global Active Power")
-dev.off()
